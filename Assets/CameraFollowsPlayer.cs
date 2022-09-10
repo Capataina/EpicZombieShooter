1fadb7cfb40e9b159c132af1594e145933bd7c15
Tulgar Dinç <tulgardinc@gmail.com>
2022-09-10 23:51:15 +0200

    better camera smoothing

--- Assets/CameraFollowsPlayer.cs
+++ Assets/CameraFollowsPlayer.cs
@@ -6,20 +6,18 @@ public class CameraFollowsPlayer : MonoBehaviour
 {
     [SerializeField]
     Transform target;
+    [SerializeField] float smoothing;
 
     Vector3 offset;
+    Vector3 cameraVelocity = Vector3.zero;
 
     void Start()
     {
         offset = transform.position - target.position;
     }
 
-    void Update()
+    void LateUpdate()
     {
-        transform.position = new Vector3(
-            target.position.x,
-            target.position.y,
-            target.position.z
-        ) + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, target.position + offset, ref cameraVelocity, smoothing);
     }
 }
