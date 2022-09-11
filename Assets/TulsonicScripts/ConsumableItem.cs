47d1d52b72a60d7396db894a3f66bb75626ea6e9
Tulgar Dinç <tulgardinc@gmail.com>
2022-09-11 22:53:57 +0200

    start

---
+++ Assets/TulsonicScripts/ConsumableItem.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Consumable", menuName = "Items/Consumable Item")]
+public class ConsumableItem : ItemBase
+{
+
+}
---
+++ Assets/TulsonicScripts/ItemBase.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBase : ScriptableObject
+{
+    [SerializeField] public float itemId;
+    [SerializeField] public string itemName;
+}
